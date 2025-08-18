059e852c6436545470e9a24ce55baff0d1bebad0
Hanson Zhuang <hansonzhuang@outlook.com>
2025-08-18 11:45:56 +0100

    complete task four

--- src/main/java/com/jpmc/midascore/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/TransactionListener.java
@@ -3,6 +3,7 @@ package com.jpmc.midascore;
 import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.foundation.Incentive;
 import com.jpmc.midascore.repository.TransactionRepository;
 import com.jpmc.midascore.repository.UserRepository;
 import org.slf4j.Logger;
@@ -10,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class TransactionListener {
@@ -20,6 +22,9 @@ public class TransactionListener {
 
     @Autowired
     private TransactionRepository transactionRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @KafkaListener(topics = "transactions", groupId = "midas-core")
     public void listen(Transaction transaction) {
@@ -36,14 +41,36 @@ public class TransactionListener {
             return;
         }
 
+        Incentive incentive = restTemplate.postForObject(
+                "http://localhost:8080/incentive",
+                transaction,
+                Incentive.class
+        );
+
+        float incentiveAmount = (incentive != null) ? incentive.getAmount() : 0f;
+
+
         sender.setBalance(sender.getBalance() - transaction.getAmount());
-        recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+        recipient.setBalance(recipient.getBalance() + transaction.getAmount() + incentiveAmount);
         userRepository.save(sender);
         userRepository.save(recipient);
 
-        TransactionRecord record = new TransactionRecord(sender, recipient, transaction.getAmount());
+        TransactionRecord record = new TransactionRecord(sender, recipient, transaction.getAmount(), incentiveAmount);
         transactionRepository.save(record);
 
-        logger.info("Transaction recorded: {}", transaction);
+        logger.info("Transaction recorded with incentive {}: {}", incentiveAmount, transaction);
+
+        UserRecord a = userRepository.findById(1L);
+        UserRecord b = userRepository.findById(2L);
+        UserRecord c = userRepository.findById(3L);
+        UserRecord d = userRepository.findById(4L);
+        UserRecord e = userRepository.findById(5L);
+        UserRecord f = userRepository.findById(6L);
+        UserRecord g = userRepository.findById(7L);
+        UserRecord h = userRepository.findById(8L);
+        UserRecord i = userRepository.findById(9L);
+        UserRecord j = userRepository.findById(10L);
+        logger.info("{}{}{}{}{}{}{}{}{}{}", a, b,c,d,e,f,g,h,i,j);
+
     }
 }
--- src/main/java/com/jpmc/midascore/entity/TransactionRecord.java
+++ src/main/java/com/jpmc/midascore/entity/TransactionRecord.java
@@ -21,14 +21,18 @@ public class TransactionRecord {
     @Column(nullable = false)
     private float amount;
 
+    @Column(nullable = false)
+    private float incentive;
+
     private LocalDateTime timestamp;
 
     protected TransactionRecord() {}
 
-    public TransactionRecord(UserRecord sender, UserRecord recipient, float amount) {
+    public TransactionRecord(UserRecord sender, UserRecord recipient, float amount, float incentive) {
         this.sender = sender;
         this.recipient = recipient;
         this.amount = amount;
+        this.incentive = incentive;
         this.timestamp = LocalDateTime.now();
     }
 
@@ -37,4 +41,7 @@ public class TransactionRecord {
     public UserRecord getRecipient() { return recipient; }
     public float getAmount() { return amount; }
     public LocalDateTime getTimestamp() { return timestamp; }
+
+    public float getIncentive() { return incentive; }
+    public void setIncentive(float incentive) { this.incentive = incentive; }
 }
