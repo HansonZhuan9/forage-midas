d053a2853b84263d86d5f05fff14411cce71a901
Hanson Zhuang <hansonzhuang@outlook.com>
2025-08-18 11:56:15 +0100

    complete task five

---
+++ src/main/java/com/jpmc/midascore/BalanceController.java
@@ -0,0 +1,20 @@
+package com.jpmc.midascore;
+
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class BalanceController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/balance")
+    public Balance getBalance(@RequestParam Long userId) {
+        return userRepository.findById(userId)
+                .map(user -> new Balance(user.getBalance()))
+                .orElse(new Balance(0f));
+    }
+}
